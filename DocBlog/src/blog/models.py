30bd2eb7453908cdcbd00c690088f045c80e890a
LaureSaintAntonin <lsaint_antonin@yahoo.fr>
2023-10-20 13:49:36 +0200

    Création des models, mise à jour de la BDD,

--- DocBlog/src/blog/models.py
+++ DocBlog/src/blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User  # Assurez-vous d'importer User depuis le bon emplacement
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)  # Utilisation de models. CASCADE comme exemple
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
