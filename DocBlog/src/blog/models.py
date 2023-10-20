b37539c71281a7e6eedb67425025182353c813d8
LaureSaintAntonin <lsaint_antonin@yahoo.fr>
2023-10-20 15:20:56 +0200

    amélioration des liens entre les fichiers

--- DocBlog/src/blog/models.py
+++ DocBlog/src/blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User  # Assurez-vous d'importer User depuis le bon emplacement
 from django.utils import timezone
 
-class Post(models.Model):
+class Article(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)  # Utilisation de models. CASCADE comme exemple
     title = models.CharField(max_length=200)
     text = models.TextField()
--- DocBlog/src/blog/views.py
+++ DocBlog/src/blog/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Article
 
 # Create your views here
 def index(request):
+	# Exemple de données à transmettre au modèle
+	#context = {
+		#'titre': 'Mon Blog',
+		#'articles': ['Article 1', 'Article 2', 'Article 3']
+		# Vous pouvez remplacer ceci par les vraies données de votre blog
+	#}
+	#return render(request, "blog/index.html", context)
 	return render(request, "blog/index.html")
 
 def article(request, numero_article):
-	return render(request, f"blog/article_{numero_article}.html")
+	article = Article.objects.filter(published_date__lte=timezone.now()).order_by('published_date')[numero_article - 1]
+	return render(request, f"blog/article_{numero_article}.html", {'article': article})
